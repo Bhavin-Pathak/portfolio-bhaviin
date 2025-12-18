9d64e8e4e3f6ed719dcacaa1bbda211d71db0d04
Bhavin-Pathak  <bhavinpathak29@gmail.com>
2025-12-18 15:28:20 +0530

    feat: update only DARK-theme for portfolio

--- src/theme/theme-provider.js
+++ src/theme/theme-provider.js
@@ -1,28 +1,15 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect } from "react";
 
 const ThemeContext = createContext();
 
 // eslint-disable-next-line react/prop-types
 export function ThemeProvider({ children }) {
-  const [isDark, setIsDark] = useState(false);
-
   useEffect(() => {
-    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-    setIsDark(prefersDark);
+    document.documentElement.classList.add("dark");
   }, []);
 
-  useEffect(() => {
-    if (isDark) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
-  }, [isDark]);
-
-  const toggleTheme = () => setIsDark(!isDark);
-
   return (
-    <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+    <ThemeContext.Provider value={{ isDark: true, toggleTheme: () => { } }}>
       {children}
     </ThemeContext.Provider>
   );
